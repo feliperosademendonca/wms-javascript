c33ac15b5406f6b74da0d4305d32a7d0fde9da4b
felipe <107950074+feliperosademendonca@users.noreply.github.com>
2022-11-13 06:40:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let endereço1 ={
+    produto:"produto1",
+    nome:"produto1",
+    validade:01/01/23,
+    rua:"a",
+    lado: "impar",
+    predio: 1,
+    andar: 1,
+    
+}
+
+console.log(endereço1.validade)
+
+let produtos_list [
+    
+]
